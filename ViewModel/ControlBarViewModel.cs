32feaff5f23efc6722cf8b2d8d7cf1d2a7f3b874
thanhnhanvo192 <102230035@sv1.dut.udn.vn>
2025-03-30 17:22:54 +0700

    First upload

---
+++ ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QuanLyPhongTro.ViewModel
+{
+    public class ControlBarViewModel : BaseViewModel
+    {
+        #region commands
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
+        public ICommand MouseMoveWindowCommand { get; set; }
+        #endregion
+
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>(
+                (p) => p != null,
+                (p) =>
+                {
+                    Window parentWindow = Window.GetWindow(p);
+                    if (parentWindow != null)
+                    {
+                        parentWindow.Close();
+                    }
+                });
+
+            MaximizeWindowCommand = new RelayCommand<UserControl>(
+                (p) => p != null,
+                (p) =>
+                {
+                    Window parentWindow = Window.GetWindow(p);
+                    if (parentWindow != null)
+                    {
+                        if (parentWindow.WindowState != WindowState.Maximized)
+                        {
+                            parentWindow.WindowState = WindowState.Maximized;
+                        }
+                        else
+                        {
+                            parentWindow.WindowState = WindowState.Normal;
+                        }
+                    }
+                });
+
+            MinimizeWindowCommand = new RelayCommand<UserControl>(
+                (p) => p != null,
+                (p) =>
+                {
+                    Window parentWindow = Window.GetWindow(p);
+                    if (parentWindow != null)
+                    {
+                        if (parentWindow.WindowState != WindowState.Minimized)
+                        {
+                            parentWindow.WindowState = WindowState.Minimized;
+                        }
+                        else
+                        {
+                            parentWindow.WindowState = WindowState.Maximized;
+                        }
+                    }
+                });
+
+            MouseMoveWindowCommand = new RelayCommand<UserControl>(
+                (p) => p != null,
+                (p) =>
+                {
+                    Window parentWindow = Window.GetWindow(p);
+                    if (parentWindow != null)
+                    {
+                        parentWindow.DragMove();
+                    }
+                });
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyPhongTro.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded = false;
+        #region
+        public ICommand LoadedWindowCommand { get; set; }
+        public ICommand RoomWindowCommand { get; set; }
+        public ICommand TenantWindowCommand { get; set; }
+        public ICommand BillCommand { get; set; }
+        #endregion
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<UserControl>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    IsLoaded = true;
+                    LoginWindow loginWindow = new LoginWindow();
+                    loginWindow.ShowDialog();
+                });
+            RoomWindowCommand = new RelayCommand<UserControl>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    RoomWindow roomWindow = new RoomWindow();
+                    roomWindow.ShowDialog();
+                });
+            TenantWindowCommand = new RelayCommand<UserControl>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    TenantWindow tenantWindow = new TenantWindow();
+                    tenantWindow.ShowDialog();
+                });
+            BillCommand = new RelayCommand<UserControl>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    BillWindow billWindow = new BillWindow();
+                    billWindow.ShowDialog();
+                });
+        }
+    }
+}
